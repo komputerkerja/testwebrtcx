c2a13e310d946400ba026ecf94589f8307d0f928
komputerkerja <komputerkerja100@gmail.com>
2022-07-23 14:40:51 +0700

    upgrade update webrtc vc

---
+++ public/app.js
@@ -0,0 +1,67 @@
+const uservideo = document.getElementById('user-video')
+const remotevideo = document.getElementById('remote-video')
+const socket = io()
+let peer;
+let otherId;
+
+socket.on('other join', async data => {
+    otherId=data.filter(id=>socket.id!=id)[0]
+    await createPeerConnection()
+})
+
+socket.on('offer', async ({offer, from}) => {
+    otherId = from
+    await createPeerConnection()
+    await peer.setRemoteDescription(new RTCSessionDescription(offer))
+    const answer = await peer.createAnswer()
+    socket.emit('answer', ({answer,from:socket.id,to:otherId}))
+    await peer.setLocalDescription(new RTCSessionDescription(answer))
+})
+
+socket.on('answer', async ({answer}) => {
+    await peer.setRemoteDescription(new RTCSessionDescription(answer))  
+})
+
+socket.on('ice candidate', async ({candidate}) => {
+    try{
+        await peer.addIceCandidate(new RTCIceCandidate(candidate))
+        console.log('success add candidate')
+    }catch{
+        console.warn('failed on add candidate')
+    }
+})
+
+socket.on('call ended', () => window.location.href = "/")
+
+async function createPeerConnection(){
+    peer = new RTCPeerConnection({iceServers:[{urls:'stun:stun.stunprotocol.org'}]})
+    const streamUserVideo = await navigator.mediaDevices.getUserMedia({video:true,audio:true})
+    uservideo.muted=true
+    uservideo.srcObject=streamUserVideo
+    uservideo.onloadedmetadata = () => uservideo.play()
+    streamUserVideo.getTracks().forEach(track=>peer.addTrack(track,streamUserVideo))
+    // handle peer events
+    peer.onicecandidate=handleIceCandidate
+    peer.ontrack=handleTrack
+    peer.onnegotiationneeded=handleNegotiationneeded
+}
+
+async function handleNegotiationneeded(){
+    const offer = await peer.createOffer()
+    socket.emit('offer',{offer,from:socket.id,to:otherId})
+    peer.setLocalDescription(new RTCSessionDescription(offer))
+}
+
+async function handleTrack(e){
+    const [stream] = e.streams
+    remotevideo.muted = true
+    remotevideo.srcObject = stream
+    remotevideo.onloadedmetadata = () => remotevideo.play()
+}
+
+async function handleIceCandidate(e){
+    socket.emit('ice candidate', {candidate:e.candidate, to: otherId})
+}
+
+uservideo.onclick = e => uservideo.muted = !uservideo.muted
+remotevideo.onclick = e => remotevideo.muted = !remotevideo.muted
--- server.js
+++ server.js
@@ -3,28 +3,19 @@ const app = express()
 const server = require('http').Server(app)
 const socketIO = require('socket.io')
 const io = socketIO(server)
-const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
-app.get('/', (req,res) => res.render('index.html'))
+app.get('/',(req,res)=>res.render('index.html'))
 
 io.on('connection', socket => {
     socket.join('room1')
-    const allSocketInRoom = io.sockets.adapter.rooms.get('room1')
-    if(allSocketInRoom.size > 1) socket.emit('other join',[...allSocketInRoom])
+    const roomLength = io.sockets.adapter.rooms.get('room1')
+    if(roomLength.size > 1) socket.emit('other join', [...roomLength])
 
-    socket.on('offer', data => {
-        socket.to(data.to).emit('offer', {offer:data.offer, from: data.from})
-    })
-
-    socket.on('answer', data => {
-        socket.to(data.to).emit('answer',{answer:data.answer, from: data.from})
-    })
-
-    socket.on('ice candidate', data => {
-        socket.to(data.to).emit('ice candidate', {candidate: data.candidate})
-    })
-
+    socket.on('offer', ({offer,from,to}) => socket.to(to).emit('offer',({offer,from})))
+    socket.on('answer', ({answer, from, to}) => socket.to(to).emit('answer', ({answer,from})))
+    socket.on('ice candidate', ({candidate, to}) => socket.to(to).emit('ice candidate',({candidate})))
+    socket.on('disconnect', () => io.emit('call ended'))
 })
 
-server.listen(PORT, () => console.log(`http://localhost:${PORT}`))
+server.listen(3000,()=>console.log(`http://localhost:${3000}`))
